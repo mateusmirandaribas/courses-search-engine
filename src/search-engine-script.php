5d9c1bfdc7acea45b3f05373d207e75f03df1e50
mateusmirandaribas <contato.mateusmribas@gmail.com>
2024-08-21 23:02:51 -0300

    refactor: adding exception handling

--- src/search-engine-script.php
+++ src/search-engine-script.php
@@ -7,12 +7,16 @@ use src\DisplayCourses;
 use src\Http\SearchEngineClient;
 use Symfony\Component\DomCrawler\Crawler;
 
-$searchEngineClient = new SearchEngineClient();
-$searchCourses = new SearchCourses($searchEngineClient);
+try {
+    $searchEngineClient = new SearchEngineClient();
+    $searchCourses = new SearchCourses($searchEngineClient);
 
-$html = $searchCourses->search('cursos-online-programacao/php');
+    $html = $searchCourses->search('cursos-online-programacao/php');
 
-$crawler = new Crawler();
-$displayCourses = new DisplayCourses($crawler);
+    $crawler = new Crawler();
+    $displayCourses = new DisplayCourses($crawler);
 
-$displayCourses->show($html);
+    $displayCourses->show($html);
+} catch (Exception $exception) {
+    echo $exception->getMessage() . $exception->getCode() . PHP_EOL;
+}
